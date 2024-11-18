5d49b16f0fded7337d0ad853aa520d2a6afd7aba
krola1 <kwl082024@gmail.com>
2024-11-18 09:22:32 +0100

    First commit, set up html css and assigned constants

---
+++ main.js
@@ -0,0 +1,6 @@
+const toArray = document.querySelector("#to-array");
+const displayArray = document.querySelector("#display-array");
+const clear = document.querySelector("#clear-array");
+const input = document.querySelector("#input");
+const listHeader = document.querySelector("#list-header");
+const theList = [];
